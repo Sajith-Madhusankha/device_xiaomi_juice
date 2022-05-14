cbbb6f454638b12a745d31384002d9fd6e25f4bf
Sajith Madhusankha <85398592+Sajith-Madhusankha@users.noreply.github.com>
2022-05-14 09:41:49 +0530

    Update arrow_juice.mk

--- arrow_juice.mk
+++ arrow_juice.mk
@@ -9,11 +9,6 @@ $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common cipher stuff.
 $(call inherit-product, vendor/arrow/config/common.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Gapps
-TARGET_GAPPS_ARCH := arm64
-USE_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := arrow_juice
